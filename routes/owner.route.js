b1e004ef29c16eb63431b38a230c5df0f30fe8bf
Priya <priya27092003@gmail.com>
2024-11-21 21:03:34 +0530

    third commit

--- routes/owner.route.js
+++ routes/owner.route.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const ownerModel = require('../models/onwer.model');
 
 router.get("/", function (req, res) {
     res.send("hello there");
+})
+
+router.post("/create", async (req, res)=>{
+    let owners = await ownerModel.find();
+    if(owners.length > 0){
+        return res
+                .status(503)
+                .send("You don't have permission to create a new owner")
+    }
+    let {fullname, email, password} = req.body;
+    let createdOwner = await ownerModel.create({
+        fullname: fullname,
+        email: email,
+        password: password
+    })
+    res.status(200).send(createdOwner);
 })
 
 module.exports = router;
